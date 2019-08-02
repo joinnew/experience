327118f07998018b70f357dee915655281eabb78
joinnew <1772629008@qq.com>
2019-08-02 15:13:22 +0800

    [add]输入框及时搜索

---
+++ demo01/src/components/inputDebounce.vue
@@ -0,0 +1,38 @@
+<script>
+  export default {
+    data () {
+      return {
+        inputValue: '',
+        timer: null
+      }
+    },
+
+    watch: {
+      inputValue (val) {
+        if (!val) {} // 当内容清空时候的操作
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+
+        this.timer = setTimeout(() => {
+          this.setSearchData(val)
+        }, 500) // 在用户输入内容结束后500ms之后，就会发送请求
+      }
+    },
+
+    methods: {
+      setSearchData (val) { // 请求数据的函数
+
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="input-debounce">
+    <el-input
+      v-model="inputValue"
+      palceholder="输入内容进行搜索"
+    ></el-input>
+  </div>
+</template>
