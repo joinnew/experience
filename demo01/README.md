4b833f1e9c6f08a6738332d7e23c5e3d607a4ed0
joinnew <1772629008@qq.com>
2019-08-03 21:18:11 +0800

    [add]增加网页内容复制到剪切板上的功能

--- demo01/README.md
+++ demo01/README.md
@@ -6,3 +6,4 @@
 #### components文件中就是放置代码的地方
 - fileDownloadPost.vue是用于处理业务中存在post请求文件的情况，请求成功的话， 拿到的就是二进制流的结果
 - inputDebounce.vue是用于处理输入框需要及时搜索，匹配数据。可以考虑使用节流操作
+- clickCopyText.vue是用于复制文本内容到剪切板上的。
---
+++ demo01/src/components/clickCopyText.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    methods: {
+      // 复制内容到剪切板上
+      handleCopy(content = '') { // 实际中有关数据内部的部分一般是通过变量渲染出来的，到时候把要复制的内容传递过来就可以了。
+        let text = content
+        let oInput = document.createElement('input')
+        oInput.value = text
+        document.body.appendChild(oInput)
+        oInput.select() // 选择对象;
+        document.execCommand("Copy") // 执行浏览器复制命令
+        this.shortcuts.msg('已成功复制到剪切板')
+        oInput.remove()
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="click-copy-text">
+    <p>内容部分
+      <el-tooltip content="复制">
+        <i class="el-icon-document-copy" @click="handleCopy"></i> 
+      </el-tooltip>
+    </p>
+  </div>
+</template>
